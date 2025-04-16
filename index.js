6584f609e032ef6dd4bccdbd3b5287869b68e83e
Azmeer Hassan Ammad <azmeerhassanammad@gmail.com>
2025-04-16 18:50:09 +0500

    PUT API

--- index.js
+++ index.js
@@ -44,6 +44,21 @@ app.post('/api/products', async(req, res)=>{
     
 })
 
+app.put('/api/product/:id', async(req, res)=>{
+    try {
+        const {id} = req.params
+        
+        const product = await Product.findByIdAndUpdate(id, req.body)
+        if(!product) return res.status(404).json({message: 'Product not found'})
+
+            const updatedProduct = await Product.findById(id)
+            res.status(200).json(updatedProduct)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+        
+    }
+})
+
 
 
 mongoose.connect('mongodb+srv://azmeerhassanammad:yUDwsCYQB91nrOJd@cluster0.cloqafa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
