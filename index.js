e97fb53aa410985c853297ef2b2e5ed015b1bdd3
Azmeer Hassan Ammad <azmeerhassanammad@gmail.com>
2025-04-16 10:40:33 +0500

    MongoDB setup

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
 app.get('/', (req, res)=>{
-    res.send('Hello from Node API')
+    res.send('Hello From Node API')
 })
 
 
 
-app.listen(3000, ()=>{
-    console.log('Server is running on port 3000'); 
+
+
+mongoose.connect('mongodb+srv://azmeerhassanammad:yUDwsCYQB91nrOJd@cluster0.cloqafa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+.then(()=>{
+    console.log('Connected to MongoDB Atlas');
+    app.listen(3000, ()=>{
+        console.log('Server is running on port 3000'); 
+    }) 
+})
+.catch((err)=>{
+    console.log('Error connecting to MongoDB Atlas', err); 
 })
