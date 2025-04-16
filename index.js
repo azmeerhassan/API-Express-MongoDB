bf5d58de3f186dab091f2ae32adf26fe5051221c
Azmeer Hassan Ammad <azmeerhassanammad@gmail.com>
2025-04-16 11:18:51 +0500

    GET API

--- index.js
+++ index.js
@@ -9,6 +9,30 @@ app.get('/', (req, res)=>{
     res.send('Hello From Node API')
 })
 
+app.get('/api/products', async(req, res)=>{
+    try{
+        const products = await Product.find()
+        res.status(200).json(products)
+    }
+    catch(error)
+    {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.get('/api/products/:id', async(req, res)=>{
+    try{
+        const product = await Product.findById(req.params.id)
+        if(!product) return res.status(404).json({message: 'Product not found'})
+
+            res.status(200).json(product)
+    }
+    catch(error)
+    {
+        res.status(500).json({message: error.message})
+    }
+})
+
 app.post('/api/products', async(req, res)=>{
     try{
         const product = await Product.create(req.body)
